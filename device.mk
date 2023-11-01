1cda9b08724f853445a4328a7ce79be8bb647680
Alone0316 <rutujamandlik06@gmail.com>
2023-11-01 19:04:06 -0300

    onclite: Build libstdc++.so from source 03-03 03:41:40.321  631    631 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library  "libstdc++.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -398,6 +398,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libstdc++ \
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
